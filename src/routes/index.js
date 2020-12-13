36b413604350c2d1225545ef964643ba0b1c5e5b
clacla-s8 <clariceks8@gmail.com>
2020-12-12 23:22:05 -0300

    index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.status(200).json({
-        name: "API Alugue Aí",
-        versao: "1.0"
+        nome: "API Alugue Aí",
+        versao: "1.0",
+        descricao: "A Alugue Aí, tem por objetivo conectar pessoas que tenham objetos ociosos em casa e gostariam de coloca-los para alugar à pessoas que têm o intesse em alugar estes objetos",
+        Autora: "Clarice Santos"
     })
 });
 
